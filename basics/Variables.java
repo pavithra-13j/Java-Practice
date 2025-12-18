cc262728adc23a5794b24d3aef62b0c104bbc09f
Pavithra Chikkala <pavithracsd09@gmail.com>
2025-12-18 20:15:16 +0530

    Add Variables Demo
    Initial implementation of Variables class with main method to display id, marks, and grade.

---
+++ basics/Variables.java
@@ -0,0 +1,12 @@
+class Variables 
+{
+  public static void main(String args[])
+  {
+    int id=9;
+    double marks=99.9;
+    char grade='A';
+    System.out.println("id");
+    System.out.println("marks");
+    System.out.println("grade");
+  }
+}
