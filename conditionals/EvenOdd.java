5b73cb120c452f3b7d4c2aef83c9f0d79eada3e9
Pavithra Chikkala <pavithracsd09@gmail.com>
2025-12-18 19:25:31 +0530

    Add EvenOdd program

---
+++ conditionals/EvenOdd.java
@@ -0,0 +1,14 @@
+class EvenOdd {
+  public static void main(String args[])
+  {
+    int num=9;
+    if(num%2==0)
+    {
+      System.out.println("EVEN");
+    }
+    else 
+    {
+      System.out.println("ODD");
+    }
+  }
+}
