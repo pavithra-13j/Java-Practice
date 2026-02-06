b8b95465bedc46f27e6b5b78c11f2c8ba205c82d
Pavithra Chikkala <pavithracsd09@gmail.com>
2026-02-06 23:10:15 +0530

    Implement PrintArray class to display array elements
    Prints elements of an array starting from index 1.

---
+++ Arrays/PrintArray.java
@@ -0,0 +1,10 @@
+class PrintArray {
+  public static void main(String args[])
+  {
+    int arr[]={10,20,30,40};
+    for(int i=1;i<arr.length;i++)
+      {
+        System.out.println(arr[i]);
+      }
+  }
+}
